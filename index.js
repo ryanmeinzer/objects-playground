129782f931ff357e3545fe99ade5087e84bcde86
Ryan Meinzer <ryanmeinzer@ryanmeinzer.com>
2020-12-10 21:10:29 -0800

    displaying object properties

--- index.js
+++ index.js
@@ -7,3 +7,18 @@ let dog = {
 }
 
 console.log(dog.nameAndAge())
+
+// display object property values with loop
+function display() {
+    for (property in dog) {
+        console.log(dog[property])
+    }
+}
+
+display()
+
+// display object property values in/converted-to an array with Object.values()
+console.log(Object.values(dog))
+
+// display object property values in/converted-to a JSON string with JSON.stringify()
+console.log(JSON.stringify(dog))
